699fa374030aa98866e634efb376f446e2a12908
KirstyWright <kirsty@evecmdr.com>
2021-02-28 11:07:29 +0000

    Whoopsie

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ class Bot(discord.Client):
                     return
                 subscription = subscription[1]
                 count = self.storage.select_multiple(
-                    "SELECT * FROM subscriptions ORDER BY id desc;")
+                    "SELECT * FROM subscriptions WHERE process_name = ? ORDER BY id desc;", [subscription])
                 if (len(count) == 0):
                     self.storage.query("INSERT INTO subscriptions (process_name, channel_id, server_id) VALUES (?, ?, ?)", [
                                        subscription, message.channel.id, message.channel.guild.id])
